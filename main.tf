b51adda4642f4064c57a77e4065cffc3044ee326
visicool <00vishalvaibhav@gmail.com>
2023-05-09 01:21:35 +0530

    added new config file

---
+++ main.tf
@@ -0,0 +1,18 @@
+    terraform {
+      backend "remote" {
+        # The name of your Terraform Cloud organization.
+        organization = "Terraform-Module-repo"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+          name = "TF-module main branch"
+        }
+      }
+    }
+
+    # An example resource that does nothing.
+    resource "null_resource" "example" {
+      triggers = {
+        value = "A example resource that does nothing!"
+      }
+    }
