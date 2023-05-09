ba25639a980641a4adcf5c77c01a9183a3538b7e
Vishal Singh <68348103+VisiCool@users.noreply.github.com>
2023-05-09 14:45:12 +0530

    modified TF cloud worksapce

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
+#     terraform {
+#       backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "example-organization"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "example-workspace"
+#         }
+#       }
+#     }
+#
+#     # An example resource that does nothing.
+#     resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
+=======
     terraform {
       backend "remote" {
         # The name of your Terraform Cloud organization.
@@ -5,7 +25,7 @@
 
         # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
-          name = "TF-module main branch"
+          name = "TF-module_main_branch"
         }
       }
     }
@@ -16,3 +36,4 @@
         value = "A example resource that does nothing!"
       }
     }
+>>>>>>> 654f11c907d4c00940318a6c573926a505829f7e
